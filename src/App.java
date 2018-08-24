ceb2d3197275ef3d603c4c98c55728b9eba78784
Srinivas <srinivas.aleti03@gmail.com>
2018-08-24 16:58:06 +0530

    Add a main method to run the app

--- src/App.java
+++ src/App.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import context.ContextHolder;
+import controller.GameController;
 
 public class App {
+    public static void main(String[] args) {
+        int totalScore = 40;
+        int totalOvers = 4;
+        GameController gameController = new GameController(totalOvers, totalScore, ContextHolder.getTeam());
+
+        gameController.start();
+    }
 }
